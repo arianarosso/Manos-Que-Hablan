4624dc4a24ee885c93803d3418f02395bc343d4f
ARIANA MARIA ROSSO <42565648@ort>
2017-07-07 11:03:34 -0300

    Unir flechas terminado

---
+++ UnirConFlechas/UnirConFlechas/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace UnirConFlechas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.pcb2 = new System.Windows.Forms.PictureBox();
+            this.pcb3 = new System.Windows.Forms.PictureBox();
+            this.pcbR2 = new System.Windows.Forms.PictureBox();
+            this.pcbR4 = new System.Windows.Forms.PictureBox();
+            this.pcbR1 = new System.Windows.Forms.PictureBox();
+            this.pcbR3 = new System.Windows.Forms.PictureBox();
+            this.btnComprobar = new System.Windows.Forms.Button();
+            this.pcbFondo1 = new System.Windows.Forms.PictureBox();
+            this.pcbFondo2 = new System.Windows.Forms.PictureBox();
+            this.pcbFondo3 = new System.Windows.Forms.PictureBox();
+            this.pcbFondoR3 = new System.Windows.Forms.PictureBox();
+            this.pcbFondoR2 = new System.Windows.Forms.PictureBox();
+            this.pcbFondoR1 = new System.Windows.Forms.PictureBox();
+            this.pcbFondoR4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcb1
+            // 
+            this.pcb1.BackColor = System.Drawing.Color.Transparent;
+            this.pcb1.Location = new System.Drawing.Point(73, 33);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(101, 93);
+            this.pcb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb1.TabIndex = 0;
+            this.pcb1.TabStop = false;
+            this.pcb1.Click += new System.EventHandler(this.pcb1_Click);
+            // 
+            // pcb2
+            // 
+            this.pcb2.BackColor = System.Drawing.Color.Transparent;
+            this.pcb2.Location = new System.Drawing.Point(73, 176);
+            this.pcb2.Name = "pcb2";
+            this.pcb2.Size = new System.Drawing.Size(101, 93);
+            this.pcb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb2.TabIndex = 0;
+            this.pcb2.TabStop = false;
+            this.pcb2.Click += new System.EventHandler(this.pcb2_Click);
+            // 
+            // pcb3
+            // 
+            this.pcb3.BackColor = System.Drawing.Color.Transparent;
+            this.pcb3.Location = new System.Drawing.Point(73, 335);
+            this.pcb3.Name = "pcb3";
+            this.pcb3.Size = new System.Drawing.Size(101, 93);
+            this.pcb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb3.TabIndex = 0;
+            this.pcb3.TabStop = false;
+            this.pcb3.Click += new System.EventHandler(this.pcb3_Click);
+            // 
+            // pcbR2
+            // 
+            this.pcbR2.Location = new System.Drawing.Point(707, 123);
+            this.pcbR2.Name = "pcbR2";
+            this.pcbR2.Size = new System.Drawing.Size(101, 93);
+            this.pcbR2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbR2.TabIndex = 0;
+            this.pcbR2.TabStop = false;
+            this.pcbR2.Click += new System.EventHandler(this.pcbR2_Click);
+            // 
+            // pcbR4
+            // 
+            this.pcbR4.Location = new System.Drawing.Point(707, 373);
+            this.pcbR4.Name = "pcbR4";
+            this.pcbR4.Size = new System.Drawing.Size(101, 93);
+            this.pcbR4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbR4.TabIndex = 0;
+            this.pcbR4.TabStop = false;
+            this.pcbR4.Click += new System.EventHandler(this.pcbR4_Click);
+            // 
+            // pcbR1
+            // 
+            this.pcbR1.Location = new System.Drawing.Point(707, 12);
+            this.pcbR1.Name = "pcbR1";
+            this.pcbR1.Size = new System.Drawing.Size(101, 93);
+            this.pcbR1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbR1.TabIndex = 0;
+            this.pcbR1.TabStop = false;
+            this.pcbR1.Click += new System.EventHandler(this.pcbR1_Click);
+            // 
+            // pcbR3
+            // 
+            this.pcbR3.Location = new System.Drawing.Point(707, 240);
+            this.pcbR3.Name = "pcbR3";
+            this.pcbR3.Size = new System.Drawing.Size(101, 93);
+            this.pcbR3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbR3.TabIndex = 0;
+            this.pcbR3.TabStop = false;
+            this.pcbR3.Click += new System.EventHandler(this.pcbR3_Click);
+            // 
+            // btnComprobar
+            // 
+            this.btnComprobar.Location = new System.Drawing.Point(399, 426);
+            this.btnComprobar.Name = "btnComprobar";
+            this.btnComprobar.Size = new System.Drawing.Size(75, 23);
+            this.btnComprobar.TabIndex = 1;
+            this.btnComprobar.Text = "Comprobar";
+            this.btnComprobar.UseVisualStyleBackColor = true;
+            this.btnComprobar.Click += new System.EventHandler(this.btnComprobar_Click);
+            // 
+            // pcbFondo1
+            // 
+            this.pcbFondo1.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondo1.Location = new System.Drawing.Point(65, 25);
+            this.pcbFondo1.Name = "pcbFondo1";
+            this.pcbFondo1.Size = new System.Drawing.Size(116, 110);
+            this.pcbFondo1.TabIndex = 2;
+            this.pcbFondo1.TabStop = false;
+            this.pcbFondo1.Visible = false;
+            // 
+            // pcbFondo2
+            // 
+            this.pcbFondo2.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondo2.Location = new System.Drawing.Point(65, 168);
+            this.pcbFondo2.Name = "pcbFondo2";
+            this.pcbFondo2.Size = new System.Drawing.Size(116, 108);
+            this.pcbFondo2.TabIndex = 2;
+            this.pcbFondo2.TabStop = false;
+            this.pcbFondo2.Visible = false;
+            // 
+            // pcbFondo3
+            // 
+            this.pcbFondo3.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondo3.Location = new System.Drawing.Point(65, 327);
+            this.pcbFondo3.Name = "pcbFondo3";
+            this.pcbFondo3.Size = new System.Drawing.Size(116, 109);
+            this.pcbFondo3.TabIndex = 2;
+            this.pcbFondo3.TabStop = false;
+            this.pcbFondo3.Visible = false;
+            // 
+            // pcbFondoR3
+            // 
+            this.pcbFondoR3.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondoR3.Location = new System.Drawing.Point(699, 231);
+            this.pcbFondoR3.Name = "pcbFondoR3";
+            this.pcbFondoR3.Size = new System.Drawing.Size(116, 112);
+            this.pcbFondoR3.TabIndex = 2;
+            this.pcbFondoR3.TabStop = false;
+            this.pcbFondoR3.Visible = false;
+            // 
+            // pcbFondoR2
+            // 
+            this.pcbFondoR2.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondoR2.Location = new System.Drawing.Point(699, 114);
+            this.pcbFondoR2.Name = "pcbFondoR2";
+            this.pcbFondoR2.Size = new System.Drawing.Size(116, 112);
+            this.pcbFondoR2.TabIndex = 3;
+            this.pcbFondoR2.TabStop = false;
+            this.pcbFondoR2.Visible = false;
+            // 
+            // pcbFondoR1
+            // 
+            this.pcbFondoR1.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondoR1.Location = new System.Drawing.Point(699, 3);
+            this.pcbFondoR1.Name = "pcbFondoR1";
+            this.pcbFondoR1.Size = new System.Drawing.Size(116, 112);
+            this.pcbFondoR1.TabIndex = 3;
+            this.pcbFondoR1.TabStop = false;
+            this.pcbFondoR1.Visible = false;
+            // 
+            // pcbFondoR4
+            // 
+            this.pcbFondoR4.BackColor = System.Drawing.Color.LimeGreen;
+            this.pcbFondoR4.Location = new System.Drawing.Point(699, 364);
+            this.pcbFondoR4.Name = "pcbFondoR4";
+            this.pcbFondoR4.Size = new System.Drawing.Size(116, 112);
+            this.pcbFondoR4.TabIndex = 2;
+            this.pcbFondoR4.TabStop = false;
+            this.pcbFondoR4.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "fkcseahgdgf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UnirConFlechas.Properties.Resources.Background;
+            this.ClientSize = new System.Drawing.Size(887, 492);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnComprobar);
+            this.Controls.Add(this.pcbR3);
+            this.Controls.Add(this.pcbR4);
+            this.Controls.Add(this.pcb3);
+            this.Controls.Add(this.pcbR1);
+            this.Controls.Add(this.pcbR2);
+            this.Controls.Add(this.pcb2);
+            this.Controls.Add(this.pcb1);
+            this.Controls.Add(this.pcbFondo3);
+            this.Controls.Add(this.pcbFondoR4);
+            this.Controls.Add(this.pcbFondoR3);
+            this.Controls.Add(this.pcbFondo2);
+            this.Controls.Add(this.pcbFondoR1);
+            this.Controls.Add(this.pcbFondo1);
+            this.Controls.Add(this.pcbFondoR2);
+            this.Name = "Form1";
+            this.Text = "Unir con flechas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbR3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFondoR4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.PictureBox pcb2;
+        private System.Windows.Forms.PictureBox pcb3;
+        private System.Windows.Forms.PictureBox pcbR2;
+        private System.Windows.Forms.PictureBox pcbR4;
+        private System.Windows.Forms.PictureBox pcbR1;
+        private System.Windows.Forms.PictureBox pcbR3;
+        private System.Windows.Forms.Button btnComprobar;
+        private System.Windows.Forms.PictureBox pcbFondo1;
+        private System.Windows.Forms.PictureBox pcbFondo2;
+        private System.Windows.Forms.PictureBox pcbFondo3;
+        private System.Windows.Forms.PictureBox pcbFondoR3;
+        private System.Windows.Forms.PictureBox pcbFondoR2;
+        private System.Windows.Forms.PictureBox pcbFondoR1;
+        private System.Windows.Forms.PictureBox pcbFondoR4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
